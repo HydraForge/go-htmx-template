d0a4728facb970deb4cc6b878d83695cec5b14c8
zack <zb01406@gmail.com>
2024-02-20 20:31:56 -0500

    adds chi & templ

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module hydraforge/go-htmx-template
 
 go 1.22.0
+
+require (
+	github.com/a-h/templ v0.2.543 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hydraforge/go-htmx-template/frontend/src/components"
+	"log"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
+	r := chi.NewRouter()
+	r.Get("/", templ.Handler(components.Home()).ServeHTTP)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 	fmt.Println("works")
 }
